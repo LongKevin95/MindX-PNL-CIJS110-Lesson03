ce82886151204de5ba82b672389a18f22cfa8911
KevinLong <long.luong.contact@gmail.com>
2026-02-27 19:11:59 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/MindX-PNL-CIJS110-Lesson0git3/",
+  base: "/MindX-PNL-CIJS110-Lesson03/",
 });
